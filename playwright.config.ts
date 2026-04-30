c67d2c68494ade78958b7e5d6517326b4874d5e3
Deepak Thakur <thakurdpk786@gmail.com>
2026-04-30 10:58:20 +0530

    fixed reporting

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,6 +23,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
+    ['html', {outputFolder: 'playwright-report'}],
     ['json', {outputFile: 'test-results/jsonReport.json'}],
     ['junit', {outputFile: 'test-results/junitReport.xml'}]
   ],
